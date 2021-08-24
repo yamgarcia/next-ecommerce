e42a884bca8afc7bedd68cbf58bfe96be0a77c6f
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2021-08-23 23:20:55 -0700

    Add external mock files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { InferGetStaticPropsType } from "next";
+import getAllProducts from "../framework/shopify/product/get-all-products";
 
 export async function getStaticProps() {
-  const products = [1, 2, 3, 4];
+  const products = await getAllProducts();
 
   return {
     props: {
